3af4a82b1c512e5a442e6f69040de879572387a8
SaiAkella <sai.akella@3plearning.com>
2021-03-07 16:13:33 +1100

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,32 @@
+## Introduction
+This is a simple weather app which takes geographical co-ordinates i.e. latitude and longitude of a place as input and outputs the following data for the next <n> days (including current day respective to timezone):
+
+* no of days temperature is predicted to be more than <n> degrees and weather conditions are predicted to be sunny
+
+## Technologies used
+This app is created with:
+* Plain/Vanilla JavaScript
+* HTML
+* CSS
+
+## Setup
+Setting up this app is pretty simple. All you need is a browser, except IE.
+
+Fetch API which is part of JavaScript is not supported on IE and requires a polyfill.
+
+For simplicity purposes, polyfill code is not handled in the app.
+
+## Usage
+Clone the repo on your machine and follow the below instructions: 
+* Open the `index.html` file under the root folder
+* Input latitude and langitude of geolocation for which the calculated weather  data to be retrieved 
+    (For Sydney, "lat": -33.8679, "lon": 151.2073)
+* Enter `OpenWeatherMap API Key` and click `FetchForecastData` button
+
+Note: For filter criteria such as no of days to be considered for forecast , weather condition, tempearature are set in `data.json` file. Respective values can be changed to retrieve different data. Please be mindful `One Call API` provides forecast for 7 days and historical weather data for previous 5 days 
+
+## Considerations
+* From the lists of weather condition codes provided by openweathermap (https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2), picked code `Clear` as Sunny
+
+* As part of free subscription, `One Call API` and `5 Day / 3 Hour Forecast` provides 5 day forecast. `One Call API` provides an option to exclude hourly, minutely data and hence, fixed that as part of the logic
+
---
+++ fetchWeatherForecast.js
@@ -0,0 +1,77 @@
+// Fetch elements on the page
+const form = document.getElementById('weather');
+const latitude = document.getElementById('lat');
+const longitude = document.getElementById('lon');
+const apiKey = document.getElementById('api_key');
+const temperatureDetails = document.getElementById('temp')
+const weatherConditionDetails = document.getElementById('weather_condition');
+
+// Fetch filter criteria
+let filterCriteria;
+fetch('./data.json')
+.then(response => response.json())
+.then((data) => {
+    filterCriteria = data;
+})
+.catch(err => console.error('Error reading properties from JSON file'))
+
+/*
+Fetch `daily` weather forecast updates for the inputted data 
+and output the calculated data
+*/
+form.addEventListener('submit', (event) => {
+    const url = `http://api.openweathermap.org/data/2.5/onecall?lat=${latitude.value}&lon=${longitude.value}&exclude=hourly,current,minutely,alerts&units=metric&appid=${apiKey.value}`;
+    
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+        const { daily } = data;
+        const calculatedData = weatherCalculation(daily, filterCriteria);
+        temperatureDetails.textContent = `#days > 23 degrees predicted: ${calculatedData.expectedTemperatureDays}`;
+        weatherConditionDetails.textContent = `#days sunny weather predicted: ${calculatedData.expectedWeatherConditionDays}`;
+    })
+    .catch(err => console.error('Error fetching forecast data:', err))
+
+    event.preventDefault(); //prevents the default action of submitting the form
+});
+
+function weatherCalculation(forecastData, filterCriteria) {
+    const dailyTemperatureData = [], dailyWeatherConditionData = [];
+    let expectedTemperatureDays = 0, expectedWeatherConditionDays = 0;
+
+    for (let i = 0; i < filterCriteria.noOfDays; i++) {
+        // Store daily temperature data
+        dailyTemperatureData.push(forecastData[i].temp);
+
+        // Store daily weather condition data
+        dailyWeatherConditionData.push(forecastData[i].weather[0]);
+    }
+
+    console.log('DailyTempData', dailyTemperatureData);
+    console.log('DailyWeatherConditionData', dailyWeatherConditionData);
+
+    /*
+    Iterate through daily temperature data and identify number of days 
+    matching the criteria
+    */ 
+    dailyTemperatureData.forEach((temperature) => {
+        if (temperature.max > filterCriteria.expectedTemperature) {
+            expectedTemperatureDays++; 
+        }
+    })
+
+    /*
+    Iterate through daily weather condition data and identify number of days 
+    matching the criteria
+    */ 
+    dailyWeatherConditionData.forEach((weatherCondition) => {
+        if (weatherCondition.main === filterCriteria.expectedWeatherCondition) {
+            expectedWeatherConditionDays ++;
+        }
+    })
+
+    return {
+        expectedTemperatureDays,
+        expectedWeatherConditionDays 
+    }
+}
